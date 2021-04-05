b3a328f12a6264c9a630b3b2df0cc76d6bd0c468
Abdi-01 <abdialghi@gmail.com>
2021-04-05 10:27:03 +0700

    ikea : make add stock

--- src/components/modalProduct.js
+++ src/components/modalProduct.js
@@ -3,7 +3,6 @@ import { Button, Modal, ModalHeader, ModalBody, ModalFooter, FormGroup, Label, I
 
 class ModalProduct extends React.Component {
     constructor(props) {
-        console.log("props", props)
         super(props);
         this.state = {
             stock: []
@@ -11,21 +10,57 @@ class ModalProduct extends React.Component {
     }
 
     onBtAdd = () => {
-
+        console.log(this.state.stock)
     }
 
-    onBtAddStock=()=>{
-
+    onBtAddStock = () => {
+        let tempStock = [...this.state.stock]
+        tempStock.push({ id: null, type: null, qty: null })
+        this.setState({ stock: tempStock })
     }
 
     printStock = () => {
-        
+        if (this.state.stock.length > 0) {
+            return this.state.stock.map((item, index) => {
+                return <Row>
+                    <Col>
+                        <Input type="text" placeholder={`Type-${index + 1}`} onChange={(e) => this.handleType(e, index)} />
+                    </Col>
+                    <Col>
+                        <Input type="number" placeholder={`Stock-${index + 1}`} onChange={(e) => this.handleStock(e, index)} />
+                    </Col>
+                    <Col>
+                        <span class="material-icons-round">
+                            delete_outline
+                        </span>
+                    </Col>
+                </Row>
+            })
+        }
+    }
+
+    onBtDeleteStock = (index) => {
+        this.state.stock.splice(index, 1)
+    }
+
+    handleType = (e, index) => {
+        this.state.stock[index].type = e.target.value
+    }
+
+    handleStock = (e, index) => {
+        this.state.stock[index].qty = parseInt(e.target.value)
+    }
+
+    onBtCancel = () => {
+        this.setState({ stock: [] })
+        this.props.btClose()
     }
 
     render() {
+        console.log('ModalOpen', this.props.modalOpen)
         return (
             <Modal isOpen={this.props.modalOpen} toggle={this.props.btClose} >
-                <ModalHeader toggle={this.props.toggle}>Add Product</ModalHeader>
+                <ModalHeader toggle={this.props.btClose}>Add Product</ModalHeader>
                 <ModalBody>
                     <FormGroup>
                         <Label for="textNama">Nama Product</Label>
@@ -43,52 +78,15 @@ class ModalProduct extends React.Component {
                         <Label for="textNama">Kategori</Label>
                         <Input type="text" id="textNama" innerRef={elemen => this.inNama = elemen} />
                     </FormGroup>
-                    <Row>
-                        <Col>
-                            <FormGroup>
-                                <Label for="textNama">Type 1</Label>
-                                <Input type="text" id="textNama" innerRef={elemen => this.inType1 = elemen} />
-                            </FormGroup>
-                        </Col>
-                        <Col>
-                            <FormGroup>
-                                <Label for="textNama">Stock 1</Label>
-                                <Input type="text" id="textNama" innerRef={elemen => this.inStock1 = elemen} />
-                            </FormGroup>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <FormGroup>
-                                <Label for="textNama">Type 2</Label>
-                                <Input type="text" id="textNama" innerRef={elemen => this.inType1 = elemen} />
-                            </FormGroup>
-                        </Col>
-                        <Col>
-                            <FormGroup>
-                                <Label for="textNama">Stock 2</Label>
-                                <Input type="text" id="textNama" innerRef={elemen => this.inStock1 = elemen} />
-                            </FormGroup>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <FormGroup>
-                                <Label for="textNama">Type 3</Label>
-                                <Input type="text" id="textNama" innerRef={elemen => this.inType1 = elemen} />
-                            </FormGroup>
-                        </Col>
-                        <Col>
-                            <FormGroup>
-                                <Label for="textNama">Stock 3</Label>
-                                <Input type="text" id="textNama" innerRef={elemen => this.inStock1 = elemen} />
-                            </FormGroup>
-                        </Col>
-                    </Row>
+                    <FormGroup>
+                        <Label>Stock</Label>
+                        <Button outline color="success" type="button" size="sm" style={{ float: 'right' }} onClick={this.onBtAddStock}>Add Stock</Button>
+                        {this.printStock()}
+                    </FormGroup>
                 </ModalBody>
                 <ModalFooter>
-                    <Button color="primary" onClick={this.props.toggle}>Do Something</Button>{' '}
-                    <Button color="secondary" onClick={this.props.toggle}>Cancel</Button>
+                    <Button type="button" color="primary" onClick={this.onBtAdd}>Submit</Button>{' '}
+                    <Button color="secondary" onClick={this.onBtCancel}>Cancel</Button>
                 </ModalFooter>
             </Modal>
         );
