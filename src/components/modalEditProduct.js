287d5ea1b60e562c6c4b2e62b41facfd12ec4989
Abdi-01 <abdialghi@gmail.com>
2021-04-05 14:28:12 +0700

    ikea : detail produk

--- src/components/modalEditProduct.js
+++ src/components/modalEditProduct.js
@@ -2,15 +2,15 @@ import axios from 'axios';
 import React from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter, FormGroup, Label, Input, Row, Col } from 'reactstrap';
 import { URL_API } from '../helper'
-class ModalProduct extends React.Component {
+class ModalEditProduct extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             stock: [],
-            images: []
+            images: [],
         }
     }
-
+    
     onBtAdd = () => {
         console.log(this.state.stock)
         axios.post(URL_API + '/products', {
@@ -23,34 +23,33 @@ class ModalProduct extends React.Component {
             images: this.state.images
         }).then(res => {
             console.log(res.data)
-            this.props.getData()
             alert('Add Product Success')
         }).catch(err => {
             console.log(err)
         })
     }
-
+    
     onBtAddStock = () => {
         // let tempStock = [...this.state.stock]
         this.state.stock.push({ id: null, type: null, qty: null })
         this.setState({ stock: this.state.stock })
     }
-
-    // menambah penampung data image pada state.images
+    
     onBtAddImages = () => {
         this.state.images.push("")
         this.setState({ images: this.state.images })
     }
-
+    
     printStock = () => {
-        if (this.state.stock.length > 0) {
-            return this.state.stock.map((item, index) => {
+        let { stock } = this.props.detailProduk
+        if (stock) {
+            return stock.map((item, index) => {
                 return <Row>
                     <Col>
-                        <Input type="text" placeholder={`Type-${index + 1}`} onChange={(e) => this.handleType(e, index)} />
+                        <Input type="text" defaultValue={item.type} placeholder={`Type-${index + 1}`} onChange={(e) => this.handleType(e, index)} />
                     </Col>
                     <Col>
-                        <Input type="number" placeholder={`Stock-${index + 1}`} onChange={(e) => this.handleStock(e, index)} />
+                        <Input type="number" defaultValue={item.qty} placeholder={`Stock-${index + 1}`} onChange={(e) => this.handleStock(e, index)} />
                     </Col>
                     <Col>
                         <a onClick={() => this.onBtDeleteStock(index)} style={{ cursor: 'pointer' }}>Delete</a>
@@ -59,72 +58,85 @@ class ModalProduct extends React.Component {
             })
         }
     }
-
-    // render element input form image
+    
     printImages = () => {
-        if (this.state.images.length > 0) {
-            return this.state.images.map((item, index) => {
-                return <Input type="text" placeholder={`Images-${index + 1}`} 
-                onChange={(e) => this.handleImages(e, index)} />
+        let { images } = this.props.detailProduk
+        if (images) {
+            return images.map((item, index) => {
+                return <Input type="text" defaultValue={item} placeholder={`Images-${index + 1}`} onChange={(e) => this.handleImages(e, index)} />
             })
         }
     }
-
+    
     onBtDeleteStock = (index) => {
-        this.state.stock.splice(index, 1)
+        this.props.detailProduk.stock.splice(index, 1)
         this.setState({ stock: this.state.stock })
     }
-
-    // Untuk set value kedalam state.images
+    
     handleImages = (e, index) => {
-        this.state.images[index] = e.target.value
+        this.props.detailProduk.images[index] = e.target.value
     }
-
+    
     handleType = (e, index) => {
-        this.state.stock[index].type = e.target.value
+        this.props.detailProduk.stock[index].type = e.target.value
     }
-
+    
     handleStock = (e, index) => {
-        this.state.stock[index].qty = parseInt(e.target.value)
+        this.props.detailProduk.stock[index].qty = parseInt(e.target.value)
     }
-
+    
     onBtCancel = () => {
-        this.setState({ stock: [], images: [] })
+        this.setState({ stock: [], images: []})
         // fungsi untuk close modal
         this.props.btClose()
     }
 
+    onBtSubmit=()=>{
+        console.log(
+            {
+                nama: this.inNama.value,
+                deskripsi: this.inDeskripsi.value,
+                brand: this.inBrand.value,
+                kategori: this.inKategori.value,
+                harga: parseInt(this.inHarga.value),
+                stock: this.props.detailProduk.stock,
+                images: this.props.detailProduk.images
+            }
+        )
+    }
+    
     render() {
-        console.log('ModalOpen', this.props.modalOpen)
+        console.log("detailProduk", this.props.detailProduk)
+        let { nama, deskripsi, brand, kategori, harga } = this.props.detailProduk
         return (
             <Modal isOpen={this.props.modalOpen} toggle={this.props.btClose} >
                 <ModalHeader toggle={this.props.btClose}>Add Product</ModalHeader>
                 <ModalBody>
                     <FormGroup>
                         <Label for="textNama">Nama Product</Label>
-                        <Input type="text" id="textNama" innerRef={elemen => this.inNama = elemen} />
+                        <Input type="text" id="textNama" defaultValue={nama} innerRef={elemen => this.inNama = elemen} />
                     </FormGroup>
                     <FormGroup>
                         <Label for="textDes">Deskripsi</Label>
-                        <Input type="text" id="textDes" innerRef={elemen => this.inDeskripsi = elemen} />
+                        <Input type="text" defaultValue={deskripsi} id="textDes" innerRef={elemen => this.inDeskripsi = elemen} />
                     </FormGroup>
                     <Row>
                         <Col>
                             <FormGroup>
                                 <Label for="textBrand">Brand</Label>
-                                <Input type="text" id="textBrand" innerRef={elemen => this.inBrand = elemen} />
+                                <Input type="text" defaultValue={brand} id="textBrand" innerRef={elemen => this.inBrand = elemen} />
                             </FormGroup>
                         </Col>
                         <Col>
                             <FormGroup>
                                 <Label for="textKategori">Kategori</Label>
-                                <Input type="text" id="textKategori" innerRef={elemen => this.inKategori = elemen} />
+                                <Input type="text" defaultValue={kategori} id="textKategori" innerRef={elemen => this.inKategori = elemen} />
                             </FormGroup>
                         </Col>
                     </Row>
                     <FormGroup>
                         <Label for="textHarga">Harga</Label>
-                        <Input type="number" id="textHarga" innerRef={elemen => this.inHarga = elemen} />
+                        <Input type="number" defaultValue={harga} id="textHarga" innerRef={elemen => this.inHarga = elemen} />
                     </FormGroup>
                     <FormGroup>
                         <Label>Stock</Label>
@@ -138,7 +150,7 @@ class ModalProduct extends React.Component {
                     </FormGroup>
                 </ModalBody>
                 <ModalFooter>
-                    <Button type="button" color="primary" onClick={this.onBtAdd}>Submit</Button>{' '}
+                    <Button type="button" color="primary" onClick={this.onBtSubmit}>Submit</Button>{' '}
                     <Button color="secondary" onClick={this.onBtCancel}>Cancel</Button>
                 </ModalFooter>
             </Modal>
@@ -146,4 +158,4 @@ class ModalProduct extends React.Component {
     }
 }
 
-export default ModalProduct;
+export default ModalEditProduct;
--- src/pages/productManagement.js
+++ src/pages/productManagement.js
@@ -1,31 +1,94 @@
+import axios from 'axios';
 import React from 'react';
-import { Button, Table } from 'reactstrap';
+import { Button, Table, Badge } from 'reactstrap';
 import ModalProduct from '../components/modalProduct';
+import ModalEditProduct from '../components/modalEditProduct';
+import { URL_API } from '../helper'
+
+let kursor = {
+    cursor: "pointer",
+    marginRight: '0.5vw'
+}
 
 class ProductManagement extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            modalOpen: false
+            modalOpen: false,
+            modalEditOpen: false,
+            data: [],
+            detailProduk: {},
+            thumbnail: 0
         }
     }
 
-    // 
+    componentDidMount() {
+        this.getData()
+    }
+
+    getData = () => {
+        axios.get(URL_API + '/products')
+            .then(res => {
+                this.setState({ data: res.data })
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    printProduk = () => {
+        return this.state.data.map((item, index) => {
+            return <tr>
+                <td>{index + 1}</td>
+                <td style={{ width: '20vw', textAlign: 'center' }}>
+                    {
+                        this.state.thumbnail[0] == index ?
+                            <img src={item.images[this.state.thumbnail[1]]} width="80%" alt={item.nama + index} />
+                            :
+                            <img src={item.images[0]} width="80%" alt={item.nama + index} />
+                    }
+                    <div>
+                        {
+                            item.images.map((value, idx) => {
+                                return <img src={value} style={kursor} width="20%" alt={item.nama + idx}
+                                    onClick={() => this.setState({ thumbnail: [index, idx] })} />
+                            })
+                        }
+                    </div>
+                </td>
+                <td>{item.nama}</td>
+                <td>{item.brand}</td>
+                <td>{item.kategori}</td>
+                <td>{
+                    item.stock.map((item, index) => {
+                        return <h5>{item.type} : <Badge color={item.qty >= 12 ? "success" : "warning"}>{item.qty}</Badge></h5>
+                    })
+                }</td>
+                <td>Rp. {item.harga.toLocaleString()}</td>
+                <td><Button type="button" size="sm" color="warning" onClick={() => this.setState({ detailProduk: item, modalEditOpen: !this.state.modalEditOpen })}>Detail</Button>
+                    <Button size="sm" color="danger">Delete</Button></td>
+            </tr>
+        })
+    }
 
     render() {
+        console.log(this.state.detailProduk)
         return (
             <div className="p-2">
                 <h3 className="text-center">Produk Management</h3>
                 <Button type="button" style={{ float: 'right' }} color="success" onClick={() => this.setState({ modalOpen: !this.state.modalOpen })}>Add</Button>
-                <ModalProduct modalOpen={this.state.modalOpen} 
-                btClose={() => this.setState({ modalOpen: !this.state.modalOpen })} />
+                {/* Modal untuk detail product */}
+                <ModalEditProduct modalOpen={this.state.modalEditOpen} detailProduk={this.state.detailProduk}
+                    btClose={() => this.setState({ modalEditOpen: !this.state.modalEditOpen })} />
+                {/* Modal untuk add product */}
+                <ModalProduct modalOpen={this.state.modalOpen}
+                    btClose={() => this.setState({ modalOpen: !this.state.modalOpen })} getData={this.getData} />
                 <Table>
                     <thead>
                         <tr>
                             <th>#</th>
                             <th>Product</th>
                             <th>Nama</th>
-                            <th>Deskripsi</th>
                             <th>Brand</th>
                             <th>Kategori</th>
                             <th>Stok</th>
@@ -34,9 +97,7 @@ class ProductManagement extends React.Component {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td></td>
-                        </tr>
+                        {this.printProduk()}
                     </tbody>
                 </Table>
             </div>
